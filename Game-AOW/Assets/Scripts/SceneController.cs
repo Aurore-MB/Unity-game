a342c048ea05fe2ff0c9277baada6cf64396701e
Aurore-MB <153558810+Aurore-MB@users.noreply.github.com>
2024-06-03 00:24:16 +0200

    Update SceneController.cs

--- Game-AOW/Assets/Scripts/SceneController.cs
+++ Game-AOW/Assets/Scripts/SceneController.cs
@@ -5,7 +5,7 @@ public class SceneController : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        GameDataManager.instance.SaveGameData(); // Sauvegarder les données avant de charger une nouvelle scène
+        GameDataManager.instance.SaveGameData();
         SceneManager.LoadScene(sceneName);
     }
 }
