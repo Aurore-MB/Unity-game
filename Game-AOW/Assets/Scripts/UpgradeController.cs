93bf23821d9b87c5236d3dd821020ad388bac2af
Aurore-MB <153558810+Aurore-MB@users.noreply.github.com>
2024-06-03 00:28:39 +0200

    Update UpgradeController.cs

--- Game-AOW/Assets/Scripts/UpgradeController.cs
+++ Game-AOW/Assets/Scripts/UpgradeController.cs
@@ -3,21 +3,21 @@ using UnityEngine.UI;
 
 public class UpgradeController : MonoBehaviour
 {
-    public Canvas upgradeCanvas; // Référence au Canvas des mises à niveau
-    public Button upgradeButton; // Référence au bouton "Upgrades"
+    public Canvas upgradeCanvas; 
+    public Button upgradeButton; 
 
     void Start()
     {
-        // Assurez-vous que le Canvas des mises à niveau est désactivé au début
+    
         upgradeCanvas.gameObject.SetActive(false);
 
-        // Ajouter un listener pour le clic sur le bouton "Upgrades"
+       
         upgradeButton.onClick.AddListener(ToggleUpgradeCanvas);
     }
 
     void ToggleUpgradeCanvas()
     {
-        // Activer ou désactiver le Canvas des mises à niveau
+       
         upgradeCanvas.gameObject.SetActive(!upgradeCanvas.gameObject.activeSelf);
     }
 }
